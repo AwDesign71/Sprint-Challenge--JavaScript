b6c2da645cbfbdac9f325f8b178f8bc1bd8747e1
bigdre4557@gmail.com <amwilson2019@gmail.com>
2019-06-14 11:44:46 -0400

    completed work on univ array

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -74,7 +74,10 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+for (let i = 0; i < graduates.length; i++) {
+  universities.push(graduates[i].university);
+}
+console.log(universities.sort())
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -83,6 +86,9 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for(let i=0; i < graduates.length; i++) {
+  contactInfo.push(graduates[i])
+}
 console.log(contactInfo);
 
 
